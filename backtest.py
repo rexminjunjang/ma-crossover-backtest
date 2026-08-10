6bed29c636286652e70cb2133f93289012ef5bdd
rexminjunjang <rexminjunjang58@gmail.com>
2026-08-10 17:34:51 +0900

    Add 20/50 day moving average columns to the data frame

--- backtest.py
+++ backtest.py
@@ -1,4 +1,7 @@
 import yfinance as yf
 
 data = yf.download("AAPL", start="2019-01-01", end="2024-01-01")
-print(data.head())
+
+#Moving Average columns
+data["MA20"] = data["Close"].rolling(window=20).mean()
+data["MA50"] = data["Close"].rolling(window=50).mean()
